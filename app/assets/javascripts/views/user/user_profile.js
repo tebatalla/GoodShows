f49206696d45ed05a1d0bf8f3d05affecb1e1b20
Tomas Batalla <tebatalla@gmail.com>
2015-04-20 12:12:00 -0700

    User feed loader own element

--- app/assets/javascripts/views/user/user_profile.js
+++ app/assets/javascripts/views/user/user_profile.js
@@ -28,7 +28,7 @@ GoodShows.Views.UserProfile = Backbone.CompositeView.extend({
     }
 
     this.feed.fetch().then( function () {
-      this.$('.user-feed').removeClass('loader');
+      this.$('.user-feed-loader').remove();
     }.bind(this));
 
     this.listenTo(this.model, "sync", this.render);
@@ -60,7 +60,7 @@ GoodShows.Views.UserProfile = Backbone.CompositeView.extend({
   addFeedItem: function(feedEvent) {
     var itemView;
 
-    this.$('.user-feed').removeClass('loader');
+    this.$('.user-feed-loader').remove();
 
     if(feedEvent.get('type') === "Review") {
       itemView = new GoodShows.Views.ReviewEventItem({
